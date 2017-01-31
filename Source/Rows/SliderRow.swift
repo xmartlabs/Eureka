a946a4f1cd59b441f6770ef323780b233f98fea7
Martin Barreto <barreto@barreto.lan>
2017-01-31 14:26:24 -0300

    minor fix

--- Source/Rows/SliderRow.swift
+++ Source/Rows/SliderRow.swift
@@ -101,8 +101,9 @@ open class SliderCell: Cell<Float>, CellType {
             
         } else {
             contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-vPadding-[slider]-vPadding-|", options: NSLayoutFormatOptions.alignAllLeft, metrics: metrics, views: views))
+            contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-hPadding-[slider]-hPadding-|", options: NSLayoutFormatOptions.alignAllLastBaseline, metrics: metrics, views: views))
         }
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-hPadding-[slider]-hPadding-|", options: NSLayoutFormatOptions.alignAllLastBaseline, metrics: metrics, views: views))
+
     }
     
     func valueChanged() {
