acc2044cbd5531bb2fffd7c2f91c346c46d77427
Martin Barreto <martin@xmartlabs.com>
2017-07-12 18:41:00 -0300

    fix unit tests.

--- Source/Core/Section.swift
+++ Source/Core/Section.swift
@@ -199,7 +199,7 @@ open class Section {
     var hiddenCache = false
 }
 
-extension Section : MutableCollection, BidirectionalCollection {
+extension Section: MutableCollection, BidirectionalCollection {
 
     // MARK: MutableCollectionType
 
@@ -239,20 +239,12 @@ extension Section : MutableCollection, BidirectionalCollection {
         }
     }
 
-    public func index(after i: Int) -> Int {return i + 1}
-    public func index(before i: Int) -> Int {return i - 1}
+    public func index(after i: Int) -> Int { return i + 1 }
+    public func index(before i: Int) -> Int { return i - 1 }
 
 }
 
-/// To add `RangeReplaceableCollection` conformance to your custom collection,
-/// add an empty initializer and the `replaceSubrange(_:with:)` method to your
-/// custom type. `RangeReplaceableCollection` provides default implementations
-/// of all its other methods using this initializer and method. For example,
-/// the `removeSubrange(_:)` method is implemented by calling
-/// `replaceSubrange(_:with:)` with an empty collection for the `newElements`
-/// parameter. You can override any of the protocol's required methods to
-/// provide your own custom implementation.
-extension Section : RangeReplaceableCollection {
+extension Section: RangeReplaceableCollection {
 
     // MARK: RangeReplaceableCollectionType
 
@@ -270,7 +262,6 @@ extension Section : RangeReplaceableCollection {
         }
     }
 
-    // where C.Iterator.Element == BaseRow {
     public func replaceSubrange<C>(_ subrange: Range<Int>, with newElements: C) where C : Collection, C.Element == BaseRow {
         for i in subrange.lowerBound..<subrange.upperBound {
             if let row = kvoWrapper.rows.object(at: i) as? BaseRow {
