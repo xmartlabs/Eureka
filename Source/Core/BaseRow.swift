bc46e0560d22134b5eb941c379e16044a070c997
Marco Betschart <marco@mandelkind.swiss>
2017-12-02 15:53:29 +0100

    Made var section open
    In order to allow it to be overwritten by a row which subclasses it. This is needed for the new custom row `SplitRow`. `SplitRow` enables to put two Eureka rows side by side into one UITableViewCell.

--- Source/Core/BaseRow.swift
+++ Source/Core/BaseRow.swift
@@ -35,7 +35,7 @@ open class BaseRow: BaseRowType {
     var callbackOnCellHighlightChanged: (() -> Void)?
     var callbackOnRowValidationChanged: (() -> Void)?
     var _inlineRow: BaseRow?
-    
+
     var _cachedOptionsData: Any?
 
     public var validationOptions: ValidationOptions = .validatesOnBlur
@@ -98,18 +98,18 @@ open class BaseRow: BaseRowType {
     public var isHidden: Bool { return hiddenCache }
 
     /// The section to which this row belongs.
-    public weak var section: Section?
-	
-	public var trailingSwipeConfiguration: SwipeConfiguration?
-	
-	//needs the accessor because if marked directly this throws "Stored properties cannot be marked potentially unavailable with '@available'"
-	private var _leadingSwipeConfiguration: SwipeConfiguration?
-	
-	@available(iOS 11,*)
-	public var leadingSwipeConfiguration: SwipeConfiguration?{
-		get{ return self._leadingSwipeConfiguration }
-		set{ self._leadingSwipeConfiguration = newValue }
-	}
+    open weak var section: Section?
+
+	   public var trailingSwipeConfiguration: SwipeConfiguration?
+
+  	//needs the accessor because if marked directly this throws "Stored properties cannot be marked potentially unavailable with '@available'"
+  	private var _leadingSwipeConfiguration: SwipeConfiguration?
+
+  	@available(iOS 11,*)
+  	public var leadingSwipeConfiguration: SwipeConfiguration?{
+  		get{ return self._leadingSwipeConfiguration }
+  		set{ self._leadingSwipeConfiguration = newValue }
+  	}
 
     public required init(tag: String? = nil) {
         self.tag = tag
