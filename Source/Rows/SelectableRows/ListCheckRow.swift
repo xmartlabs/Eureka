3b7c4485f1f640dc4bfe16888d8c12d2dd421185
Karl von Randow <karl@xk72.com>
2018-03-30 21:06:18 +1300

    Use tintAdjustmentMode instead of setting the tintColor
    Setting the tintColor explicitly means this component will ignore future parent tintColor changes, which means it won’t work nicely in an app that uses tintColors.

--- Source/Rows/SelectableRows/ListCheckRow.swift
+++ Source/Rows/SelectableRows/ListCheckRow.swift
@@ -39,13 +39,11 @@ open class ListCheckCell<T: Equatable> : Cell<T>, CellType {
         accessoryType = row.value != nil ? .checkmark : .none
         editingAccessoryType = accessoryType
         selectionStyle = .default
-        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
-        tintColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
         if row.isDisabled {
-            tintColor = UIColor(red: red, green: green, blue: blue, alpha: 0.3)
+            tintAdjustmentMode = .dimmed
             selectionStyle = .none
         } else {
-            tintColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+            tintAdjustmentMode = .automatic
         }
     }
 
