39a0a22c6aa43f394df703f465af9efd51c37160
supermonk <ykphuah@gmail.com>
2019-06-11 00:03:58 +0800

    Updated with dark mode.

--- Source/Rows/TextAreaRow.swift
+++ Source/Rows/TextAreaRow.swift
@@ -80,6 +80,7 @@ open class _TextAreaCell<T> : Cell<T>, UITextViewDelegate, AreaCell where T: Equ
         textView.font = .preferredFont(forTextStyle: .body)
         textView.textContainer.lineFragmentPadding = 0
         textView.textContainerInset = UIEdgeInsets.zero
+        textView.backgroundColor = .clear
         contentView.addSubview(textView)
 
         let placeholderLabel = UILabel()
@@ -133,7 +134,11 @@ open class _TextAreaCell<T> : Cell<T>, UITextViewDelegate, AreaCell where T: Equ
         textLabel?.text = nil
         detailTextLabel?.text = nil
         textView.isEditable = !row.isDisabled
-        textView.textColor = row.isDisabled ? .gray : .black
+        if #available(iOS 13.0, *) {
+            textView.textColor = row.isDisabled ? .tertiaryLabel : .label
+        } else {
+            textView.textColor = row.isDisabled ? .gray : .black
+        }
         textView.text = row.displayValueFor?(row.value)
         placeholderLabel?.text = (row as? TextAreaConformance)?.placeholder
         placeholderLabel?.isHidden = textView.text.count != 0
