87920fec422cac9ffdc2cf6d85460f676dd74151
Jonas Zaugg <jonas.zaugg@yahoo.com>
2019-06-26 19:12:56 +0200

    Adding support for SPM

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Eureka",
+    platforms: [.iOS("9.0")],
+    products: [
+        .library(name: "Eureka", targets: ["Eureka"])
+    ],
+    targets: [
+        .target(
+            name: "Eureka",
+            path: "Source"
+        ),
+        .testTarget(
+            name: "EurekaTests",
+            dependencies: ["Eureka"],
+            path: "Tests"
+        )
+    ]
+)
