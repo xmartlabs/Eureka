a8f1ec6219aff75009e5384e56285f5c16267f1b
Boris Kolozsi <boris.kolozsi@gmail.com>
2019-09-25 15:20:21 +0200

    spm definition fixes

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "Eureka", path: "Source"),
+        .target(name: "Eureka"),
     ],
     swiftLanguageVersions: [.version("5")]
 )
