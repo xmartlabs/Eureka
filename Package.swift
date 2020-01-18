42033fa894f958d0d04a59f1decbe09fcfbcdf58
Andrew Bennet <AndrewBennet@users.noreply.github.com>
2020-01-18 13:30:48 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "Eureka",
-    platforms: [.iOS("9.0")],
+    platforms: [.iOS(.v9)],
     products: [
         .library(name: "Eureka", targets: ["Eureka"])
     ],
